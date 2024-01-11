ff60eb14f8238ffcdf3bedb3df6fef18142fbd86
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2024-01-11 23:43:27 +0200

    add:Routes

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Home from "../pages/Home";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
